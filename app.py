2c4ebec3835251b7f559ecaf87b2bc43db2aacc3
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-21 06:56:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 import os
 
 nltk.download('punkt')
@@ -15,9 +15,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages' 
 #'postgres://jskteoiomwdbsd:8deac3f1a77e50bebca9b3fac1cba5ea596d7074e3808778da6d55ee55b075a8@ec2-34-236-103-63.compute-1.amazonaws.com:5432/dah1ks5gv211h4'
 
-db = SQLAlchemy()
 
-#db.init_app(app)
+db.init_app(app)
 
 #Database model
 class SMSMessage(db.Model):
