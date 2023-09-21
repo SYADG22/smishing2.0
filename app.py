f53ebdd9317b461e840422ac9948d85399058856
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-21 05:06:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") 
 #'postgres://jskteoiomwdbsd:8deac3f1a77e50bebca9b3fac1cba5ea596d7074e3808778da6d55ee55b075a8@ec2-34-236-103-63.compute-1.amazonaws.com:5432/dah1ks5gv211h4'
 
 db = SQLAlchemy(app)
