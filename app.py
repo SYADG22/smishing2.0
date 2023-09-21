f9696e09deba07030cc70d6b7b5376c35d9ff221
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-21 07:51:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 nltk.download('punkt')
 nltk.download('stopwords')
